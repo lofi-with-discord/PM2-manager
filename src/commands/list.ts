f480b870a5f65cea1dcd8ec26d26566c093f3e91
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-05-16 17:25:09 +0900

    fix: method is wrong

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -3,6 +3,6 @@ import Client from '../classes/Client'
 import pm2 from 'pm2'
 
 export default (client: Client, msg: Message) =>
-  pm2.restart('LofiGirl', (_, proc) => msg.channel.send(`\`\`\`json\n${JSON.stringify(proc, null, 2)}\`\`\``))
+  pm2.list((err, proc) => msg.channel.send(`\`\`\`json\n${JSON.stringify(err?.message || proc, null, 2)}\`\`\``))
 
 export const aliases = ['list']
--- src/commands/restart.ts
+++ src/commands/restart.ts
@@ -1,8 +1,9 @@
 import { Message } from 'discord.js'
 import Client from '../classes/Client'
 import pm2 from 'pm2'
+import Query from '../classes/Query'
 
-export default (client: Client, msg: Message) =>
-  pm2.list((_, data) => msg.channel.send(`\`\`\`json\n${JSON.stringify(data, null, 2)}\`\`\``))
+export default (client: Client, msg: Message, query: Query) =>
+  pm2.restart(query.args[0], (err, data) => msg.channel.send(`\`\`\`json\n${JSON.stringify(err?.message || data, null, 2)}\`\`\``))
 
 export const aliases = ['list']
