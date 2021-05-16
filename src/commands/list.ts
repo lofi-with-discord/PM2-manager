c9b63084c5e6bc0dde6696fa285c3047152288c0
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2021-05-16 17:45:21 +0900

    feat: pm2 list embed

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,8 +1,26 @@
-import { Message } from 'discord.js'
+import { EmbedField, Message } from 'discord.js'
 import Client from '../classes/Client'
 import pm2 from 'pm2'
 
 export default (client: Client, msg: Message) =>
-  pm2.list((err, proc) => msg.channel.send(`\`\`\`json\n${JSON.stringify(err?.message || proc, null, 2)}\`\`\``))
+  pm2.list((err, proc) => msg.channel.send(null, {
+    embed: {
+      color: err?.message ? 0xff0000 : 0x00ff00,
+      title: err?.message ? 'Error' : 'Result',
+      ...proc
+        ? {
+            fields: proc.reduce((prev, curr) => [...prev, ({
+              name: curr.name,
+              value: `pid: \`${curr.pid}\`\ncpu: \`${curr.monit?.cpu}\`\nmem: \`${curr.monit?.memory}\``
+            } as EmbedField)], [] as EmbedField[])
+          }
+        : {},
+      ...err?.message
+        ? { description: `\`\`\`${err.message}\`\`\`` }
+        : (proc.length < 1
+            ? { description: 'empty' }
+            : {})
+    }
+  }))
 
 export const aliases = ['list']
